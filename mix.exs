893b12552badf8e33ca9225e47ddb5ca4717423f
Cheezmeister <@gmail>
2016-11-17 20:20:01 -0800

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ID3v2.Mixfile do
 
   def project do
     [app: :id3v2,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

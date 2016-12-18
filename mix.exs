90f52d9ab5761941190ea5443f4fc4184671d191
Cheezmeister <@gmail>
2016-12-18 04:56:36 -0800

    Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ID3v2.Mixfile do
   def project do
     [app: :id3v2,
      version: "0.1.2",
-     elixir: "~> 1.3",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(Mix.env),
